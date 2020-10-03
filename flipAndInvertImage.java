3f321a3014a184c5f364682f6075fa452bc682eb
kyleyiyang <kyleyiyang@gmail.com>
2020-10-03 15:15:16 -0400

    Create flipAndInvertImage.java

---
+++ flipAndInvertImage.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int i=0; i<A.length; i++) {
+            int k = A[i].length;
+            for(int j = 0; j < Math.floor(k/2); ++j) {
+                if (A[i][j] == A[i][k-1-j]) {
+                    A[i][j] = (A[i][j] + 1)%2;
+                    A[i][k-1-j] = (A[i][k-1-j] + 1)%2;
+                }
+            }
+            if (k%2 == 1) {
+                int l = (int) Math.floor(k/2);
+                 A[i][l] = (A[i][l] + 1)%2;
+            }
+        }
+        return A;
+    }
+}
