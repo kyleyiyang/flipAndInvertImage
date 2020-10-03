756e1abf6f21e1675f7736cd8394f58c93811976
kyleyiyang <kyleyiyang@gmail.com>
2020-10-03 15:20:35 -0400

    Update flipAndInvertImage.java

--- flipAndInvertImage.java
+++ flipAndInvertImage.java
@@ -16,3 +16,23 @@ class Solution {
         return A;
     }
 }
+// faster and lower memory usage
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int k = A.length;
+        int l = (int) Math.floor(k/2);
+        for (int i=0; i<k; i++) {
+            for(int j = 0; j < l; ++j) {
+                if (A[i][j] == A[i][k-1-j]) {
+                    A[i][j] = (A[i][j] + 1)%2;
+                    A[i][k-1-j] = (A[i][k-1-j] + 1)%2;
+                }
+            }
+            if (k%2 == 1) {
+                 A[i][l] = (A[i][l] + 1)%2;
+            }
+        }
+        return A;
+    }
+}
